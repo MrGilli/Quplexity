97add684cb8caf8f2e304dbd1b2d7d27cfd52721
bixel <101384741+MrGilli@users.noreply.github.com>
2024-07-24 11:54:17 +1000

    Delete ASM directory

--- ASM/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream> 
-#include <cstdint>
-extern "C" uint64_t getMemorySize();
-
-int main(){
-  uint64_t memorySize = getMemorySize();
-  std::cout << "Total memory size: " << memorySize << "bytes\n";
-  return 0;
-}
