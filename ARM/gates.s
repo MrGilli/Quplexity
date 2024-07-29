27331ab58fab9f99d79ee139541c2a1a904d14c2
bixel <101384741+MrGilli@users.noreply.github.com>
2024-07-29 19:29:35 +1000

    Update gates.s

--- ARM/gates.s
+++ ARM/gates.s
@@ -1,5 +1,9 @@
 .global _pauli_X
+.global _pauli_Z
+.global _gills_hadamard2x1
+.global _gills_hadamard2x2
 .align 3
+neg_one: .float -1.0
 
 _pauli_X:
     // Define pauli_X gate:
@@ -34,3 +38,60 @@ _pauli_X:
     STR D11, [X1, #8]
 
     RET                 //Return to caller
+
+_pauli_Z:
+    //Define pauli_X gate:
+    // [1.0,  0.0]
+    // [0.0, -1.0]
+    FMOV D1, #1.0   // D1 = 1.0
+    FMOV D2, #0.0   // D2 = 0.0
+    // -1.0
+    FMOV D3, #1.0   // D3 = -1.0
+    FNEG D3, D3
+
+    // Load vector numbers:
+    LDR D5, [X0, #0] // = a
+    LDR D6, [X0, #8] // = b
+
+    // (1⋅α + 0⋅β 
+    //  0⋅α + (−1)⋅β​)=(α−β​)
+    // ROW 1:
+    FMUL D7, D1, D5 //D7 = 1.0 * 0 = 0
+    FMUL D8, D2, D6 //D8 = 0.0 * 1.0 = 0
+    FADD D9, D7, D8 //D7 = 0 + 0 = 0
+
+    //ROW 2:
+    FMUL D10, D2, D5 //D8 = 0.0 * 0.0 = 0
+    FMUL D11, D3, D6 //D9 = -1 * 1 = -1
+    FADD D12, D10, D11 //D10 = 0 + (-1) = -1
+
+    STR D9, [X1, #0] // 0
+    STR D12, [X1, #8] // 0
+
+    RET                 // Return to caller.
+    
+_gills_hadamard2x2:
+    LDR D1, [X0]
+    LDR D2, [X0, #8]
+    LDR D3, [X0, #16]
+    LDR D4, [X0, #24]
+
+    // [H] = 1/sqrt(2)
+    FMOV D5, #1.0
+    FMOV D6, #2.0
+    FSQRT D7, D6
+    FDIV D0, D5, D7
+
+    // Multiply each element of the 2x2 matrix by D0
+    FMUL D1, D1, D0
+    FMUL D2, D2, D0
+    FMUL D3, D3, D0
+    FMUL D4, D4, D0
+
+    STR D1, [X1, #0]
+    STR D2, [X1, #8]
+    STR D3, [X1, #16]
+    STR D4, [X1, #24]
+
+    // Return output 2x2 Matrix to caller.
+    RET 
