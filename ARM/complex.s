a95fc03b0247e45134b91d5b13bfcbbc8f9bf126
bixel <101384741+MrGilli@users.noreply.github.com>
2024-08-07 21:19:19 +1000

    Update complex.s
    Finished _define_imaginary, started _pauli_Y

--- ARM/complex.s
+++ ARM/complex.s
@@ -12,5 +12,22 @@ _define_imaginary:
     MOV w2, #0x69       // Load ASCII value of 'i' (0x69) into w2
     STRB w2, [x0, #1]   // Store byte from w2 into the address pointed by x0 + 1
 
+    MOV w2, #0          // Load null terminator (0x00) into w2
+    STRB w2, [x0, #2]   // Store null terminator into the address pointed by x0 + 2
+
     RET
 
+_pauli_Y:
+    // (0⋅α + -i⋅β 
+    //  (i)⋅α + 0⋅β​)=(α−β​)
+    // Y = (0 -i 
+    //      ​i 0​)
+    MOV W1, #1
+    MOV W2, #0
+    MOV W3, -1
+
+    LDR W4, [X0, #0]
+    LDR W5, [X0, #8]
+    
+    //NOT FINISHED
+    RET
