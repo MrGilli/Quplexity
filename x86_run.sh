81e3b2586355d3bc8c50575f96d73bf51d2df9c0
Xenia <81315770+gxenia@users.noreply.github.com>
2025-02-11 20:25:41 +0100

    Update x86_run.sh
    I would add-z noexecstack is used to avoid warnings

--- x86_run.sh
+++ x86_run.sh
@@ -2,6 +2,6 @@ clear
 echo "Quplexity-x86"
 
 nasm -f elf64 ./x86/gates.asm -o ./x86/gates.o
-gcc -no-pie ./x86/main.c ./x86/gates.o -o main -lm
+gcc -no-pie -z noexecstack ./x86/main.c ./x86/gates.o -o main -lm
 
 ./main
